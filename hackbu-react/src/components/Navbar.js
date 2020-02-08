a9e595859eccf4ad370437cdbc623852e8fa7fd5
Ron <laniado.ron@gmail.com>
2020-02-08 15:27:10 -0500

    Navbar component

---
+++ hackbu-react/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Navbar.css";
+
+export default class Navbar extends React.Component {
+  render() {
+    return (
+      <div className="navbar">
+        <p className="title"><strong>HackBU Project - Stock Market Buddy</strong></p>
+      </div>
+    );
+  }
+}
