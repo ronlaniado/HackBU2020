ee819ce705a7683b5b2fbecc32b7324c4c2bffdb
Ron <laniado.ron@gmail.com>
2020-02-08 18:23:04 -0500

    Imported MarketActions component

--- hackbu-react/src/App.js
+++ hackbu-react/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from "./components/Navbar";
+import MarketActions from "./components/MarketActions";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <MarketActions />
     </div>
   );
 }
